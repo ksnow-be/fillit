bc2c9d9a7c2585aba50f7580a82ace9fcedd7ec0
Karen Snow bear <ksnow-be@e4r9p9.21-school.ru>
2019-01-15 21:58:01 +0300

    Check

---
+++ check/start.sh
@@ -0,0 +1,98 @@
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/01
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/01
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/02
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/02
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/03
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/03
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/04
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/04
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/05
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/05
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/06
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/06
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/07
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/07
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/08
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/08
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/09
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/09
+echo "================\n"
+
+echo "================"
+echo "\033[32m--------------NEW-------------------"
+echo "\033[36m State\n\033[0m"
+time ./test ex/10
+echo "\n"
+echo "\033[34m Yours\n\033[0m"
+time ./fillit ex/10
+echo "================\n"
+
+echo "\n\n\n"
+echo "\033[35m============DONE====================="
+
+
+
+
+
+
